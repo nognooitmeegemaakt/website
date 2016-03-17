7a5fb8cccbe45e8ce0346326cc844204589cb325
Sander Laarhoven <lesander@xs4all.nl>
2016-03-17 11:58:41 +0100

    Fix url for backgrounds.json

--- assets/js/backgrounds.js
+++ assets/js/backgrounds.js
@@ -1,5 +1,7 @@
+---
+---
 /* Select a random background from the available images for matching pages. */
-$.getJSON("/assets/js/backgrounds.json", function(data, textStatus, jqxhr) {
+$.getJSON("{{ site.domain }}/assets/js/backgrounds.json", function(data, textStatus, jqxhr) {
   $.each(data, function(speltak, images) {
     if (location.href.indexOf(speltak) > -1  && images.length > 0) {
       var image = images[ Math.round(Math.random() * (images.length - 1)) ];
