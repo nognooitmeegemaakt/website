02ee9c32b65dc4c40f7e75c9bacaf36f7ade5d4d
Sander Laarhoven <lesander@xs4all.nl>
2016-03-18 11:32:39 +0100

    Load news from JSON.

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,4 +1,24 @@
-
+---
+---
+var months = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun",
+              "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+$.getJSON("{{ site.domain }}/assets/js/news.json", function(data) {
+  $.each(data, function(index, news_item) {
+    var date = new Date(news_item.date);
+    var month = months[ date.getMonth() ];
+    var day = date.getDate();
+    $(".news > .row").append('\
+    <div class="news-item col-xs-10 col-sm-5 col-md-6 col-lg-4"\
+    data-id="'+news_item.id+'" data-source="'+news_item.source+'"\
+    data-type="'+news_item.type+'" data-link="'+news_item.link+'">\
+      <div class="well">\
+        <h5>'+day+' '+month+'</h5>\
+        <div class="content">'+news_item.message+'</div>\
+        <div class="image"><img src="{{ site.domain }}'+news_item.media+'"></div>\
+      </div>\
+    </div>');
+  });
+});
 
 $(".news-item").on("click", function() {
   var content = $(this).find(".content").html();
