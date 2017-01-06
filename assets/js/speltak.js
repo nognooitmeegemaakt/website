22ea2abb7f1939abbff30e2276044e805f3309b9
Sander Laarhoven <sander@laarhoven.org>
2017-01-06 16:26:19 +0100

    :shipit: Use dropdown for agenda selection in menu.

--- assets/js/speltak.js
+++ assets/js/speltak.js
@@ -1,9 +1,25 @@
 $(".speltak-mobile .row > div").on("click", function() {
-  $(this).find("a")[0].click();
-});
+  $(this).find("a")[0].click()
+})
+
 $(".speltak-open a").on("click", function() {
+
+  if ($(this).parent().hasClass('type-agenda')) {
+
+    var elems = $(document).find('.speltak-mobile > .row > div')
+    for (var i = 0; i < elems.length; i++) {
+
+      var $current = $( $(elems[i]).find('a')[0] )
+      var link = $current.attr('href')
+      $current.attr('href', link.replace('groep', 'programma'))
+    }
+
+  }
+
   $(".speltak-mobile").fadeIn("fast");
-});
+
+})
+
 $(".speltak-close").on("click", function() {
-  $(".speltak-mobile").fadeOut("fast");
+  $(".speltak-mobile").fadeOut("fast")
 })
