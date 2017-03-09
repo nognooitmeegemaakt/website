9483fcb9a63ec2edba8b6490316ffe00d112aed4
Sander Laarhoven <sander@laarhoven.org>
2017-03-10 00:01:29 +0100

    Small redesign of group, agenda and contact pages.

--- assets/js/backgrounds.js
+++ assets/js/backgrounds.js
@@ -1,27 +1,33 @@
 ---
+# Backgrounds.js
 ---
 /* Select a random background from the available images for matching pages. */
-pageImages = {};
+pageImages = {}
 $.getJSON("{{ site.domain }}/assets/js/backgrounds.json", function(data, textStatus, jqxhr) {
 
   /* Set global variable of all page images. */
-  pageImages = data;
+  pageImages = data
 
   $.each(data, function(speltak, images) {
 
     if (location.href.indexOf(speltak) > -1  && images.length > 0) {
 
       /* Select a random background. */
-      var background = images[ Math.round(Math.random() * (images.length - 1)) ];
-      $("html").attr("style", "background-image: url('{{ site.domain }}/assets/img/bg/"+background+"');");
-      return;
+      var background = images[ Math.round(Math.random() * (images.length - 1)) ]
+
+      /* Add background to html tag if viewport != xs */
+      $("#content div.header").attr("style", "background-size: cover; background-image: url('{{ site.domain }}/assets/img/bg/"+background+"')")
+      //$("html").attr("style", "background-image: none !important")
+      return
     }
 
-  });
+  })
 
   /* Set default background if no background was found. */
-  var bgstyle = $("html").attr("style");
-  if (typeof bgstyle === typeof undefined || bgstyle === false) {
-    $("html").attr("style", "background-image: url('{{ site.domain }}/assets/img/bg/main.jpg');");
+  var bgstyle = $("#content div.header").attr("style")
+  if (typeof bgstyle === "undefined" || bgstyle === false) {
+    $("#content div.header").attr("style", "background-image: url('{{ site.domain }}/assets/img/bg/main.jpg')")
+    //$("html").attr("style", "background-image: none !important")
   }
-});
+
+})
